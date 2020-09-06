8a7133c605dca3243bed8d7b61a0e332fd1de92a
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-09-06 19:10:21 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |key, value|
+  value.each do |description, array|
+  array.each do |name|
+    
+  if pigeon_list.has_key?(name)
+    if pigeon_list[name].has_key?(key)
+      pigeon_list[name][key] << description.to_s 
+    else
+      pigeon_list[name][key] = [description.to_s]
+    end
+  else
+    pigeon_list[name] = {key => [description.to_s]}
+  end
+end
+end
+end
+pigeon_list
 end
